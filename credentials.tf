d76ed7a35d9b1d1ff263a8f0bc87dd222c7e6f22
Tiago Oliveira da Silva <73368199+tsilva-cloud@users.noreply.github.com>
2025-02-12 16:54:41 -0300

    Add files via upload

---
+++ credentials.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaoi6b5sxlv4z773boczybqz3h2vspvvru42jysvizl77lky22ijaq"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaanx6lsnnfrzdp74aoq3krntyjfc6v4zy67nukwavlvhwigzhn5upq"
+  fingerprint       = "ed:62:bd:bf:13:d1:2b:f4:11:2b:18:82:94:f5:10:ed"
+  private_key_path  = "/root/automations/keys/private-tiago.key"
+  region           =  "us-ashburn-1"
+}
